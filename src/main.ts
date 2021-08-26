4770dc18437c609b8b9459036017259bf5449dce
Tingluo Huang <tingluohuang@github.com>
2021-08-26 02:33:02 +0000

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import {wait} from './wait'
+import jwtDecode, {JwtPayload} from 'jwt-decode'
 
 async function run(): Promise<void> {
   try {
@@ -9,6 +10,16 @@ async function run(): Promise<void> {
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
+
+    const token1 = await core.getIDToken()
+    const decoded1 = jwtDecode<JwtPayload>(token1)
+    core.info(decoded1.sub || 'Bad')
+    core.info(decoded1.aud?.toString() || 'Bad Aud')
+
+    const token2 = await core.getIDToken('api://AzureADTokenExchange')
+    const decoded2 = jwtDecode<JwtPayload>(token2)
+    core.info(decoded2.sub || 'Bad')
+    core.info(decoded2.aud?.toString() || 'Bad Aud')
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
